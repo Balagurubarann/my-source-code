5211bb875ccab9eb22bd691db6aceb0784fa0cfc
Kuhan @ Balagurubaran <102408933+Balagurubarann@users.noreply.github.com>
2023-02-12 08:36:31 +0530

    Add files via upload

---
+++ TypeSpeed/TypeSpeed.js
@@ -0,0 +1,76 @@
+const Quote = ["Computer Science is a rapidly evolving field. In fact, to keep pace with its development", 
+"Acknowledgement is the most beautiful page in any project's final pages", "Asking the user a legal login-id performs authorization",
+"A system designed to prevent unauthorized access to or from a private network is called Firewall","October", "September", "Running"];
+
+const displayElement = document.querySelector(".quote-display");
+const quoteInputElement = document.querySelector(".quoteInput");
+const timerElement = document.querySelector(".timer");
+const scoreElement = document.querySelector(".score");
+let score = 0;
+
+function increaseScore() {
+    return score++;
+}
+
+quoteInputElement.addEventListener('input', () => {
+
+    const arrayQuote = displayElement.querySelectorAll('span');
+    const arrayValue = quoteInputElement.value.split('');
+    let correct = true;
+
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index];
+
+        if (character == null) {
+            characterSpan.classList.remove('incorrect');
+            characterSpan.classList.remove('correct');
+            correct = false;
+        }
+        
+        else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
+        } else {
+            characterSpan.classList.add('incorrect');
+            characterSpan.classList.remove('correct');
+            correct = false;
+        }
+    })
+
+    if (correct) renderNewQuotes();
+})
+
+function getQuotes() {
+
+    return Quote[Math.floor(Math.random() * Quote.length)];
+}
+
+async function renderNewQuotes() {
+
+    const quote = getQuotes();
+    displayElement.innerHTML = '';
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+        displayElement.appendChild(characterSpan);
+    })
+    quoteInputElement.value = null;
+    startTimer();
+    scoreElement.textContent = increaseScore();
+}
+
+let startTime;
+
+function startTimer() {
+    timerElement.innerText = 0;
+    startTime = new Date();
+    setInterval(() => {
+        timer.innerText = getTimerTime();
+    }, 1000);
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000);
+}
+
+renderNewQuotes();
